8c9204e7cfe05ba8b8fb4548fe1a20ad1372dc66
Internalizable <elietgm1@gmail.com>
2020-03-16 15:02:10 +0200

    Initial Commit

---
+++ maintenancemode-bungeecord/src/main/java/me/internalizable/maintenancemode/MaintenanceModeBungeeCord.java
@@ -0,0 +1,16 @@
+package me.internalizable.maintenancemode;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public final class MaintenanceModeBungeeCord extends Plugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ maintenancemode-spigot/src/main/java/me/internalizable/maintenancemode/MaintenanceModeSpigot.java
@@ -0,0 +1,17 @@
+package me.internalizable.maintenancemode;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MaintenanceModeSpigot extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
